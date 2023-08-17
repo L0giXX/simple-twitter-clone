5fb13b048bc9e1935099b93f647a352fedaae9ed
L0giXX <marc.mueller04@gmail.com>
2023-08-17 23:08:11 +0200

    deleting useless comments and clearing up prisma schema and routers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,16 +1,8 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { postRouter } from "~/server/api/routers/posts";
 import { createTRPCRouter } from "~/server/api/trpc";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   posts: postRouter,
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -20,22 +20,10 @@ declare module "next-auth" {
   interface Session extends DefaultSession {
     user: DefaultSession["user"] & {
       id: string;
-      // ...other properties
-      // role: UserRole;
     };
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
   callbacks: {
     session: ({ session, user }) => ({
@@ -56,15 +44,6 @@ export const authOptions: NextAuthOptions = {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
